d18ed3852b945741c4259b40f967a654ca0d5217
alanzorzi <ahzferreira@hotmail.com>
2024-12-04 11:13:00 -0300

    including swagger annotation

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -16,12 +16,12 @@ func NewAuthController(service serviceInterface.AuthServiceInterface) *AuthContr
 }
 
 // LoginUser allows a user to log in and obtain an authentication token.
-// @Summary User User
+// @Summary model.User.email User.email
 // @Description Allows a user to log in and receive an authentication token.
 // @Tags Authentication
 // @Accept json
 // @Produce json
-// @Param userLogin body model.User true "User login credentials"
+// @Param userLogin body model.User.email true "User login credentials"
 // @Success 200 {object} model.User "Login successful, authentication token provided"
 // @Header 200 {string} Authorization "Authentication token"
 // @Failure 403 {object} string "Error: Invalid login credentials"
--- app/controllers/user_controller.go
+++ app/controllers/user_controller.go
@@ -17,6 +17,18 @@ func NewUserController(service serviceInterface.UserServiceInterface) *userContr
 	return &userController{userService: service}
 }
 
+// GetUserById retrieves user information based on the provided user ID.
+// @Summary Find User by ID
+// @Description Retrieves user details based on the user ID provided as a parameter.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param userId path string true "ID of the user to be retrieved"
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Success 200 {object} string "User information retrieved successfully"
+// @Failure 400 {object} string "Error: Invalid user ID"
+// @Failure 404 {object} string "User not found"
+// @Router /user/{userId} [get]
 func (ctrl *userController) GetUserById(c *gin.Context) {
 	id := c.Param("id")
 	users, err := ctrl.userService.GetUserById(id)
@@ -27,6 +39,16 @@ func (ctrl *userController) GetUserById(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 }
 
+// GetAllUsers retrieves user information based on the provided user ID.
+// @Summary Find Users
+// @Description Retrieves user details.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Success 200 {object} string "User information retrieved successfully"
+// @Failure 400
+// @Router /users [get]
 func (ctrl *userController) GetAllUsers(c *gin.Context) {
 	users, err := ctrl.userService.GetAllUsers()
 	if err != nil {
@@ -36,6 +58,18 @@ func (ctrl *userController) GetAllUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, users)
 }
 
+// CreateUser Creates a new user
+// @Summary Create a new user
+// @Description Create a new user with the provided user information
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Param userRequest body model.User true "User information for registration"
+// @Success 200 {object} model.User
+// @Failure 400
+// @Failure 500
+// @Router /users [post]
 func (ctrl *userController) CreateUser(c *gin.Context) {
 	var user model.User
 	if err := c.ShouldBindJSON(&user); err != nil {
@@ -55,6 +89,19 @@ func (ctrl *userController) CreateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, user)
 }
 
+// UpdateUser updates user information with the specified ID.
+// @Summary Update User
+// @Description Updates user details based on the ID provided as a parameter.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param userId path string true "ID of the user to be updated"
+// @Param userRequest body model.User true "User information for update"
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Success 200
+// @Failure 400
+// @Failure 500
+// @Router /users/{userId} [put]
 func (ctrl *userController) UpdateUser(c *gin.Context) {
 	var user model.User
 
@@ -78,6 +125,18 @@ func (ctrl *userController) UpdateUser(c *gin.Context) {
 	c.JSON(http.StatusCreated, user)
 }
 
+// DeleteUser deletes a user with the specified ID.
+// @Summary Delete User
+// @Description Deletes a user based on the ID provided as a parameter.
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param userId path string true "ID of the user to be deleted"
+// @Success 200
+// @Param Authorization header string true "Insert your access token" default(Bearer <Add access token here>)
+// @Failure 400
+// @Failure 500
+// @Router /users/{userId} [delete]
 func (ctrl *userController) DeleteUser(c *gin.Context) {
 	id := c.Param("id")
 	err := ctrl.userService.DeleteUser(id)
